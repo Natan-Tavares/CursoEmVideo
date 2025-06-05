f5f3ceb7277c1d22b2bfb411024786ac0023388a
Natan <natantavares4982@gmail.com>
2025-06-05 12:15:05 -0300

    Resoluss

---
+++ Resoluções/ex082.py
@@ -0,0 +1,16 @@
+lista = []
+par = []
+impar = []
+resposta = ''
+while resposta != 'n':
+    n = int(input('Digite um número: '))
+    resposta = str(input('Quer continuar? [S/N] ').strip().lower())
+    lista.append(n)
+    if n % 2 == 0:
+        par.append(n)
+    else:
+        impar.append(n)
+print('-=' * 30)
+print(f'A lista completa é {lista}')
+print(f'A listaa de paares é {par}')
+print(f'A lista de ímpares é {impar}')
---
+++ Resoluções/ex083.py
@@ -0,0 +1,15 @@
+lista = []
+n = str(input('Digite a expressão: '))
+for i in n:
+    if i == '(':
+        lista.append('(')
+    elif i == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
